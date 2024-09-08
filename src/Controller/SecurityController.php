5cfae1a90bdafc60cb8b859b2d239b0dc60227ac
vhdedic <vdedic@gmail.com>
2024-09-08 09:52:03 +0200

    Redirected user to app_home if logged in for app_register and app_login routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+        
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
